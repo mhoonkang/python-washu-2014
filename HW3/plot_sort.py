2702965d03ea7c3dd5055a455a65d128d5c624d8
mhoonkang <myunghoon@wustl.edu>
2014-08-13 23:41:42 -0500

    HW3
    change

--- HW3/plot_sort.py
+++ HW3/plot_sort.py
@@ -8,21 +8,25 @@ import pylab as plt
 
 # simulation
 def sort_sim(n, iter): # n is the number of elements in the list, iter is the number of iteration.
-  sample = range(0,n)  
-  unsorted = random.sample(sample, n) # randomly arrange n numbers
-  T0 = timeit.Timer('mergesort(%s)'% unsorted, 'from __main__ import mergesort') # set timer for mergesort
-  T1 = timeit.Timer('bubblesort(%s)'% unsorted, 'from __main__ import bubblesort') # set timer for bubblesort
-  T2 = timeit.Timer('quicksort(%s)' % unsorted, 'from __main__ import quicksort') # set timer for quicksort
-  tm = T0.timeit(number=iter) # record consumed time for excuting mergesort 'iter' times
-  tb = T1.timeit(number=iter) # record consumed time for excuting bubblesort 'iter' times
-  tq = T2.timeit(number=iter) # record consumed time for excuting quicksort 'iter' times
-  return [tm, tb, tq]
+  tm = []
+  tb = []
+  tq = []
+  for i in range(iter): # do this loop 'iter' times
+    sample = range(0,n)  
+    unsorted = random.sample(sample, n) # randomly arrange n numbers
+    T0 = timeit.Timer('mergesort(%s)'% unsorted, 'from __main__ import mergesort') # set timer for mergesort
+    T1 = timeit.Timer('bubblesort(%s)'% unsorted, 'from __main__ import bubblesort') # set timer for bubblesort
+    T2 = timeit.Timer('quicksort(%s)' % unsorted, 'from __main__ import quicksort') # set timer for quicksort
+    tm.append(T0.timeit(number=1)) # record consumed time for excuting mergesort 
+    tb.append(T1.timeit(number=1)) # record consumed time for excuting bubblesort
+    tq.append(T2.timeit(number=1)) # record consumed time for excuting quicksort 
+  return [sum(tm), sum(tb), sum(tq)] # return total time for each sorting method
 
 # plot graphs for average time consumed for each sort method from size 1 to n by iterating 'iter' times
 def plot(n=500, iter=10):
-  time_merge = [] # list of average times consumed in merge sort from size 1 to n
-  time_bubble = [] # list of average times consumed in bubble sort from size 1 to n
-  time_quick = [] # list of average times consumed in quick sort from size 1 to n
+  time_merge = [] # list where average times consumed in merge sort from size 1 to n to be stored
+  time_bubble = [] # list where average times consumed in bubble sort from size 1 to n to be stored
+  time_quick = [] # list where average times consumed in quick sort from size 1 to n to be stored
   for i in range(1,n+1): # for size n data
     mtime = sort_sim(i, iter)[0] # total time for merge sort
     btime = sort_sim(i, iter)[1] # total time for bubble sort    
